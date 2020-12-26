24d6bb1115039a00e0e47fab2e94aec7a2bbe7d9
phlupp <philipp.harsch@icloud.com>
2020-12-26 23:05:16 +0100

    Update index.js

--- app/static/js/index.js
+++ app/static/js/index.js
@@ -1,4 +1,3 @@
-/*
 var socket = io.connect('http://' + document.domain + ':' + location.port);
 
 socket.on('connect', function() {
@@ -7,21 +6,4 @@ socket.on('connect', function() {
 
 socket.on('redirectIndex', function(url) {
 	window.location.href = url;
-});
-*/
-
-const io = require("socket.io")(httpServer, {
-  cors: {
-    origin: "http://" + document.domain + ":" + location.port,
-    methods: ["GET", "POST"],
-    credentials: true
-  }
-});
-
-socket.on('connect', function() {
-     console.log('Websocket connected!');
-});
-
-socket.on('redirectIndex', function(url) {
-     window.location.href = url;
 });
