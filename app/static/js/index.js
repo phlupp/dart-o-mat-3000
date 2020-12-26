fca2354f7e6744514aa67e4a9a29d08f29f2fe51
phlupp <philipp.harsch@icloud.com>
2020-12-26 23:51:40 +0100

    Update index.js

--- app/static/js/index.js
+++ app/static/js/index.js
@@ -2,13 +2,8 @@ const socket = io();
 
 //var socket = io.connect('http://' + document.domain + ':' + location.port);
 
-//socket.on('connect', function() {
-//     console.log('Websocket connected!');
-//});
-
-socket.on("hello", (arg) => {
-	console.log('Websocket connected!');
-	console.log(arg); // world
+socket.on("connect", function() {
+     console.log('Websocket connected!');
 });
 
 socket.on("redirectIndex", function(url) {
