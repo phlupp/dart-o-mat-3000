636a2f5ff060892b41644c999851764851141230
phlupp <philipp.harsch@icloud.com>
2020-12-27 02:16:42 +0100

    Update index.js

--- app/static/js/index.js
+++ app/static/js/index.js
@@ -1,19 +1,6 @@
-//const socket = io('http://' + document.domain + ':' + location.port);
-
-//var socket = io.connect('http://' + document.domain + ':' + location.port);
-
-//socket.on("connect", function() {
-//     console.log('Websocket connected!');
-//});
-
 socket.on("connect", () => {
      console.log('Websocket connected!');
      console.log(socket.id);
-});
-
-//socket.on("redirectIndex", function(url) {
-//	window.location.href = url;
-//});
 
 socket.on("redirectIndex", (url) => {
 	window.location.href = url;
