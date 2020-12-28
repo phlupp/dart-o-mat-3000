cecef03c8204bd544ca037c017fb4cb7d4d9bdc5
phlupp <philipp.harsch@cloud.com>
2020-12-28 21:02:41 +0100

    update

--- app/static/js/index.js
+++ app/static/js/index.js
@@ -1,6 +1,7 @@
 socket.on("connect", function() {
      console.log('Websocket connected!');
      console.log(socket.id);
+});
 
 socket.on("redirectIndex", function(url) {
 	window.location.href = url;
